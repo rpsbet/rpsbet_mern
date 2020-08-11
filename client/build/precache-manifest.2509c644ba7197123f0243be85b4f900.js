3b6c5d823c4af6959056383a8714fc1aeabb0290
Flash-at-DMV <wangjin8855@hotmail.com>
2020-08-11 13:36:23 +0800

    deploy Lenny's style changes

---
+++ client/build/precache-manifest.2509c644ba7197123f0243be85b4f900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef046ceeb13c460011cf147c13bcec6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3616bf34be3b629e65d9",
+    "url": "/static/css/main.d888901e.chunk.css"
+  },
+  {
+    "revision": "1631d68c3a83916b7542",
+    "url": "/static/js/2.da381afc.chunk.js"
+  },
+  {
+    "revision": "9d9fd8bb06830b6b0cf913ec47e2a80d",
+    "url": "/static/js/2.da381afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3616bf34be3b629e65d9",
+    "url": "/static/js/main.e1a33d80.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
