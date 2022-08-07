04a7e33da5b125be1edb55abee84f1fadfbe564c
Sean Hewitson <seannkiely@icloud.com>
2022-08-07 20:54:41 +0100

    RR-50: feat - started working on nodemailer to replace sendgrid

---
+++ helper/mailer.js
@@ -0,0 +1,22 @@
+import { createTransport, SendMailOptions } from 'nodemailer';
+
+const mailer = createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MAILER_USER,
+    pass: process.env.MAILER_PASSWORD
+  }
+});
+
+export const sendEmail = (to, subject, html) =>
+  mailer.sendMail({
+    from: 'support@rpsbet.io',
+    to,
+    subject,
+    html
+  });
+
+export const resetPassword = (email, name, changePasswordId) => {
+  const template = fs.read;
+  return sendEmail({ name, address: email }, 'Reset Password', ``);
+};
