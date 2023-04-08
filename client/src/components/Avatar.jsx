3749b3c39e50776064e48345693ea7af180c1c6a
= <lennybeadle@gmail.com>
2023-04-08 22:41:25 +0100

    qds

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -33,10 +33,10 @@ export default class Avatar extends Component {
     if (src === '') {
       if (darkMode) {
         return (
-          <img {...rest} src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23212121'%3E%3Cpath d='M12 2c-4.8 0-8.7 3.9-8.7 8.7s3.9 8.7 8.7 8.7 8.7-3.9 8.7-8.7S16.8 2 12 2zM3.3 12c0-4.3 3.5-7.8 7.8-7.8s7.8 3.5 7.8 7.8-3.5 7.8-7.8 7.8-7.8-3.5-7.8-7.8zm11.1 6.8c-2.1 1.5-4.8 2.4-7.7 2.4s-5.6-.8-7.7-2.4c-.5-.4-.5-1.2 0-1.6 2.1-1.5 4.8-2.4 7.7-2.4s5.6.8 7.7 2.4c.5.4.5 1.2 0 1.6z'/%3E%3C/svg%3E" alt={alt} />
+          <img {...rest} src="/img/profile-thumbnail-dark.svg" alt={alt} />
         );
       }
-      return  <img {...rest} src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23212121'%3E%3Cpath d='M12 2c-4.8 0-8.7 3.9-8.7 8.7s3.9 8.7 8.7 8.7 8.7-3.9 8.7-8.7S16.8 2 12 2zM3.3 12c0-4.3 3.5-7.8 7.8-7.8s7.8 3.5 7.8 7.8-3.5 7.8-7.8 7.8-7.8-3.5-7.8-7.8zm11.1 6.8c-2.1 1.5-4.8 2.4-7.7 2.4s-5.6-.8-7.7-2.4c-.5-.4-.5-1.2 0-1.6 2.1-1.5 4.8-2.4 7.7-2.4s5.6.8 7.7 2.4c.5.4.5 1.2 0 1.6z'/%3E%3C/svg%3E" alt={alt} />;
+      return <img {...rest} src="/img/profile-thumbnail.svg" alt={alt} />;
     }
 
     if (darkMode) {
